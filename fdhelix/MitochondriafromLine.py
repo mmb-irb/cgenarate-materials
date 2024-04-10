d7cc0a42f63ad1e493dfd4535dc35a48c659d04b
David Farré-Gil <david.farre@irbbarcelona.org>
2024-04-10 15:03:47 +0200

    Added circular script

--- fdhelix/MitochondriafromLine.py
+++ fdhelix/MitochondriafromLine.py
@@ -4,111 +4,169 @@
 # In[1]:
 
 
-inputfile = 'line_mitochondria.pdb'
-outputfile = 'mitochondriaCG.pdb'
+inputfile = 'input/line_mitochondria.pdb'
+outputfile = 'input/mitochondriaCG3.pdb'
 
 
 # In[2]:
 
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+canonical_turns=None
+delta_link=0
+target_turns=None
 
 
 # In[3]:
 
 
-ylist=[]
-with open(inputfile, 'r')  as fin:
-    for index, line in enumerate(fin):
-        ylist.append(float(line[46-8:54-8]))
+import numpy as np
 
 
 # In[4]:
 
 
+def angles(a, b, c):
+
+    ba = a - b
+    bc = c - b
+    ac = c - a
+
+    ba /= np.expand_dims(np.linalg.norm(ba, axis=-1), axis=-1)
+    bc /= np.expand_dims(np.linalg.norm(bc, axis=-1), axis=-1)
+
+    cosine_angle = (ba * bc).sum(axis=-1)
+    #     cosine_angle=np.minimum(cosine_angle,1)
+    angle = np.arccos(cosine_angle)
+    
+    rotate=np.array(((0,-1),(1,0)))
+
+    sign = np.sign((bc @ rotate.T * ac).sum(axis=-1))
+    return angle*sign
+
+
+# In[5]:
+
+
+xlist=[]
+with open(inputfile, 'r')  as fin:
+    for index, line in enumerate(fin):
+        xlist.append(float(line[46-16:54-16]))
+        
+ylist=[]
+with open(inputfile, 'r')  as fin:
+    for index, line in enumerate(fin):
+        ylist.append(float(line[46-8:54-8]))
+        
 zlist=[]
 with open(inputfile, 'r')  as fin:
     for index, line in enumerate(fin):
         zlist.append(float(line[46:55]))
 
 
-# In[5]:
-
-
-dist=max(zlist)-min(zlist)
-dist
-
-
 # In[6]:
 
 
+zdist=max(zlist)-min(zlist)
+
+x=np.array(xlist)
 y=np.array(ylist)
 z=np.array(zlist)
+
+n=len(x)
+bp=n//2
 
 
 # In[7]:
 
 
-r=y+dist/(2*np.pi)
-theta= z/dist*(2*np.pi)
-r,theta
+if not canonical_turns:
+    canonical_turns=round(bp/10.4)
+if not target_turns:
+    target_turns=canonical_turns+delta_link
 
 
 # In[8]:
+
+
+origin=originx,originy=0,0
+
+points=np.array((x,y)).T
+
+pointsini=np.array((1,0))
+pointsfin=points[[0,-1]]
+pointsori=points[[0,-1]]*0+origin
+
+angle_0,angle_last=angles(pointsini,pointsori,pointsfin)
+
+pointsini=points[:-bp-1]
+pointsfin=points[1:bp]
+pointsori=points[:-bp-1]*0+origin
+
+allangles=angles(pointsini,pointsori,pointsfin)
+
+pointsini=points[-bp-1:-bp]
+pointsfin=points[:1]
+pointsori=points[:1]*0+origin
+
+connectangle=angles(pointsini,pointsori,pointsfin)[0]
+
+
+# In[9]:
+
+
+turns=(np.sum(allangles[:]))/(2*np.pi)
+turnsconnecting=turns+connectangle/(2*np.pi)
+
+angleproportion=target_turns/turnsconnecting
+
+turns, turnsconnecting
+
+
+# In[10]:
+
+
+alldists = np.sqrt(x**2 + y**2)
+
+newangles = allangles.cumsum() * angleproportion 
+newangles = np.concatenate((newangles + angle_0, newangles[::-1]+angle_last))
+
+newx, newy = x.copy(), y.copy()
+
+newx[1:-1], newy[1:-1] = alldists[1:-1] * np.cos(
+    newangles), alldists[1:-1] * np.sin(newangles)
+
+
+# In[11]:
+
+
+x, y = newx, newy
+
+
+# In[12]:
+
+
+r=y+zdist/(2*np.pi)
+theta= z/zdist*(2*np.pi)
+r,theta
+
+
+# In[13]:
 
 
 y2, z2 = r*np.cos(theta), r*np.sin(theta)
 y2, z2
 
 
-# In[9]:
-
-
-plt.plot(z2)
-plt.plot(y2)
-
-
-# In[10]:
-
-
-a=0
-b=a+10
-d=16568
-c=d-10
-off1=-7000
-off2=-39500
-plt.scatter(z2[a:b],y2[a:b])
-plt.scatter(z2[c:d],y2[c:d])
-# plt.plot(y[a:b]+off1,z[a:b]+off2)
-plt.axis('equal')
-
-
-# In[11]:
+# In[14]:
 
 
 with open(inputfile, 'r')  as fin:
     with open(outputfile, 'w')  as fout:
         for index, line in enumerate(fin):
 #             print(line.strip())
+            line2 = line[:4] + "  {:5d}".format(index+1) + line[11:30]
+            line2 += "{:8.2f}".format(x[index]) + "{:8.2f}".format(y2[index]) + "{:8.2f}".format(z2[index]) + line[54:]
             
-            y=float(line[38:46])
-            z=float(line[46:54])
-            
-            r=y+dist/(2*np.pi)
-            theta= z/dist*2*np.pi
-            y2, z2 = r*np.cos(theta), r*np.sin(theta)
-            
-            line2 = line[:4] + "  {:5d}".format(index+1) + line[11:38]
-            line2 += "{:8.2f}".format(y2) + "{:8.2f}".format(z2) + line[54:]
-            
-            print(line2.strip())
+#             print(line2.strip())
             fout.writelines(line2)
-
-
-# In[ ]:
-
-
-
 
